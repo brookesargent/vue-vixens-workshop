8d0c25a03269d8ecd1356d0f0f41bb5eb411f255
brookesargent <brookesargent01@gmail.com>
2019-08-03 14:25:00 -0400

    commit work from vue vixens workshop

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import VueRouter from "vue-router";
+import "vuetify/dist/vuetify.min.css";
+import Home from "./views/Home";
+import Pets from "./views/Pets";
+import Favorites from "./views/Favorites";
+import store from "./store/store";
+import Form from "./views/Form";
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets },
+  { path: "/favorites", component: Favorites },
+  { path: "/form", component: Form }
+];
+const router = new VueRouter({ routes });
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
